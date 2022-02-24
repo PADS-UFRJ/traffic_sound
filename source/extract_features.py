4c165aa8fe9460fd082ed4c14835de596e6698fc
Felipe Vianna Rodrigues <felipevr98@poli.ufrj.br>
2022-02-24 16:20:25 -0300

    Stack all batches in one array and save as npy

--- source/extract_features.py
+++ source/extract_features.py
@@ -6,6 +6,7 @@ from torchvision import models
 import torchvision.transforms as transforms
 
 import cv2
+import numpy as np
 
 import os
 import os.path as pth
@@ -153,7 +154,7 @@ feature_extractor = VggFeatureExtractor( models.vgg16(pretrained=True) )
 
 batch_size = 32
 
-device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 
 ## -----------------------------------------------------
 # Corpo principal
@@ -162,10 +163,12 @@ feature_extractor.to(device)
 
 with torch.no_grad(): # destivamos o calculo de gradiente pois nao estamos treinando o modelo
 
-    for video_index in range(2,3): # len(videos_list)): # percorremos os videos para extrair as features deles
+    for video_index in range(len(videos_list)): # percorremos os videos para extrair as features deles
 
         if not pth.isdir(save_dirs[video_index]):
             os.makedirs(save_dirs[video_index])
+
+        features_list = []
 
         dataset = FramesDataset([frames_dirs[video_index]], transform=preprocess)
 
@@ -174,24 +177,53 @@ with torch.no_grad(): # destivamos o calculo de gradiente pois nao estamos trein
         print('## -----------------------------------------------------')
         print(f'dataset: {videos_list[video_index]}')
         print(f'  imgs:{len(dataset)}')
-        print(f'  batches: {len(dataset)//batch_size}')
+        print(f'  batches: {len(dataloader)}')
         print()
+        print('Calculating outputs')
 
-        for batch_index in range(len(dataset)//batch_size): # o numero de batches que serao executados
-            print(f'- {batch_index}')
+        for batch_index in range(len(dataloader)): # o numero de batches que serao executados
+            # print(f'- batch {batch_index+1}/{len(dataloader)}')
 
-            img = next(iter(dataloader)) # carregamos a proxima imagem de entrada da rede neural
+            imgs_batch = next(iter(dataloader)) # carregamos o proximo batch de imagens de entrada da rede neural
 
-            img = img.to(device) # carregamos a entrada para a GPU (caso disponivel)
+            imgs_batch = imgs_batch.to(device) # carregamos a entrada para a GPU (caso disponivel)
 
-            features = feature_extractor(img) # passamos a imagem pela rede neural, obtendo as features
+            features = feature_extractor(imgs_batch) # passamos a imagem pela rede neural, obtendo as features
 
-            features = features.cpu() # passamos o tensor para a CPU para podermos salva-lo em disco
-            # [NOTE] eh possivel salvar um tensor em disco sem passa-lo para a CPU, mas na hora de carrega-lo
-            # seria preciso lembrar em qual dispositivo (cpu/gpu) ele estava quando foi salvo.
-            # Para evitar isso, escolhi padronizar os salvamentos na cpu
+            features = features.cpu() # passamos o tensor para a CPU para podermos salva-lo em disco como array numpy
 
-            filename = f'features_batch{batch_index}.pt'
-            filepath = pth.join(save_dirs[video_index], filename)
+            fetaures = features.numpy()
 
-            torch.save(features, filepath)
+            features_list.append(features)
+
+        # features_list[0]: o primeiro batch
+        # features_list[0][0]: as features da primeira imagem do primeiro batch
+        shape = features_list[0][0].shape # o formato de um vetor de features qualquer (no caso da primeira imagem do primeiro batch)
+
+        shape = (len(dataset),) + shape # o formato final da matriz de features (cada elemento eh um vetor de features de uma imagem)
+
+        features_array = np.empty(shape) # inicalizamos o vetor de features vazio
+        # [TODO] verificar se poderiamos usar apenas np.empty(len(dataset))
+
+        print('Stacking all outputs in one array')
+
+        begin = 0
+        for i, batch in enumerate(features_list):
+            # [NOTE] o ultimo batch possui o mesmo formato dos outros, ainda que haja menos imagens nele
+            # por isso usamos o slice no batch para pegar apenas as imagens validas
+            if i == len(features_list) - 1:
+                end = len(features_array)
+            else:
+                end = (begin + len(batch))
+
+            features_array[begin:end] = batch[:end-begin]
+
+            begin = end
+
+        filename = f'{videos_list[video_index]}_features.npy'
+        filepath = pth.join(FEATURES_DIR, filename)
+
+
+        np.save(filepath, features_array)
+
+        print(f'Saved in {filepath}')
