be91e53fb702249dcd183e06973d99f729a9740c
Felipe Vianna Rodrigues <felipevr@febe.pads.ufrj.br>
2022-05-06 13:51:37 -0300

    Add colors constants

--- source/const.py
+++ source/const.py
@@ -22,3 +22,22 @@ TARGETS_DIR = pth.join(PREPROCESSED_DIR, 'targets')
 videos_list = os.listdir( pth.join(MT_DATASET_DIR, 'raw') )
 videos_list.sort()
 videos_list = [ d.replace('.','') for d in videos_list ] # lista com nomes dos videos
+
+###############################################################################
+
+# Cores bonitas para serem usadas no grafico
+# [REF] https://towardsdatascience.com/making-matplotlib-beautiful-by-default-d0d41e3534fd
+
+CB91_Blue   = '#2CBDFE'
+CB91_Green  = '#47DBCD'
+CB91_Pink   = '#F3A0F2'
+CB91_Purple = '#9D2EC5'
+CB91_Violet = '#661D98'
+CB91_Amber  = '#F5B14C'
+
+colors = [CB91_Amber,
+            CB91_Purple,
+            CB91_Green,
+            CB91_Pink,
+            CB91_Blue,
+            CB91_Violet]
