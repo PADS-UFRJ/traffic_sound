a856e1495f613960726ade0b89c2de71c0370e81
Felipe Vianna Rodrigues <felipevr@febe.pads.ufrj.br>
2022-05-06 15:25:01 -0300

    Add utils file

---
+++ source/utils.py
@@ -0,0 +1,25 @@
+
+# ---------------------------
+
+# [REF] Stack Overflow: How do you sort files numerically? - https://stackoverflow.com/a/4623518
+
+from re import split
+
+def tryint(s):
+    return int(s) if s.isdigit() else s
+
+def alphanum_key(s):
+    """
+    Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [ tryint(c) for c in split('([0-9]+)', s) ]
+
+def sort_nicely(l):
+    """
+    Sort the given list in the way that humans expect.
+        sort_nicely( ['f_2.png', 'f_1.png', 'f_10.png'] ) -> ['f_1.png', 'f_2.png', 'f_10.png']
+    """
+    return sorted(l, key=alphanum_key)
+
+# ---------------------------
